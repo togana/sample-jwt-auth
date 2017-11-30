b98132c6d30f8325d93daa5524719ccd95f35bcb
oshiro_seiya <oshiro_seiya@ca-adv.co.jp>
2017-11-30 18:21:20 +0900

    サンプルデータをDBに入れるために seed 用意した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all
+User.destroy_all
+
+User.create!(
+  name: 'てすと',
+  email: 'test@example.com',
+  password: 'passwd',
+  password_confirmation: 'passwd'
+)
+
+10.times do |i|
+  Post.create!(
+    title: "タイトル#{i}",
+    body: "中身#{i}"
+  )
+end
